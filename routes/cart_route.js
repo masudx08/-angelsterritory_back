7e37d52ad04b3a244f38eacc3dd42054d602425e
masudx08 <promasud225@gmail.com>
2022-03-26 07:59:05 +0600

    automatically generating data

--- routes/cart_route.js
+++ routes/cart_route.js
@@ -20,15 +20,16 @@ const OneDay = 1000*60*60*24
 CartRoute.post('/:id', (req, res)=>{
   if(req.body.upPool){
     CartModel.findOne({_id:req.params.id}, function(err, result){
-      result.upPool = req.body.upPool
+      result.upPool = Number(req.body.upPool) + Number(result.upPool)
       result.save(()=>{
         res.status(200).send({message:'done'})
       })
     })
   }
+
   if(req.body.downPool){
     CartModel.findOne({_id:req.params.id}, function(err, result){
-      result.downPool = req.body.downPool
+      result.downPool = Number(req.body.downPool) + Number(result.downPool)
       result.save(()=>{
         res.status(200).send({message:'done'})
       })
@@ -38,52 +39,67 @@ CartRoute.post('/:id', (req, res)=>{
 
 CartRoute.get('/', (req, res)=>{
   CartModel.find().exec((err, result)=>{
-    console.log(result)
     res.send(result)
   })
 })
-// binance.futuresMiniTickerStream( 'BTCUSDT', (res)=>{
-//   btcPrice = res.close
-//   console.log(res)
-// } );
+
+function generateCart(props){
+  binance.futuresMarkPrice(props.currency+"USDT")
+  .then(coin=>{
+    const Cart = new CartModel({
+      currency: props.currency,
+      activeTime: props.activeTime,
+      startTime: new Date(),
+      lockedPrice: coin.indexPrice,
+      upPool: 0,
+      downPool: 0
+    })
+
+    Cart.save(function(err, res){
+      if(err){
+        console.log(err)
+      }else{
+        const id = res._id+''
+        console.log('saved')
+        setTimeout(function(){
+          if(id){
+             binance.futuresMarkPrice(props.currency+"USDT")
+             .then(coin2=>{
+                CartModel.findOneAndUpdate({_id:id}, {
+                  stopTime: new Date(),
+                  closedPrice: coin2.indexPrice
+                })
+                .then(()=>{
+                  console.log('updated')
+                })
+             })
+            
+            
+          }
+        }, props.activeTime)
+      }
+    })
+
+  })
+
+}
 
 // One Minute Generate 
-// var currentId 
-// setInterval(()=>{
-//   binance.futuresMarkPrice("BTCUSDT")
-//   .then(btc=>{
-//     const Cart = new CartModel({
-//       currency: 'BTC',
-//       activeTime: OneMinute,
-//       startTime: new Date(),
-//       lockedPrice: btc.indexPrice
-//     })
 
-//     Cart.save(function(err, res){
-//       if(err){
-//         console.log(err)
-//       }else{
-//         const id = res._id+''
-//         currentId = id
-//         console.log('saved')
-//       }
-//     })
-
-//     if(currentId){
-//       const filter = {_id:currentId}
-//       const update = {
-//         stopTime: new Date(),
-//         closedPrice: btc.indexPrice
-//       }
-//       CartModel.findOneAndUpdate(filter, update)
-//       .then(res=>{
-//         console.log('updated')
-//       })
-//     }
-
-//   })
-  
-// },OneMinute)
+setInterval(()=>{
+  generateCart({
+    currency : 'BTC',
+    activeTime: OneMinute
+  })
+  generateCart({
+    currency : 'ETH',
+    activeTime: OneMinute
+  })
+  generateCart({
+    currency : 'BNB',
+    activeTime: OneMinute
+  })
+},OneMinute)
 
 
 // setInterval(()=>{},FiveMinute)
